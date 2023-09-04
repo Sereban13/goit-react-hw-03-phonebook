56fb0b612345ee95ca2f5f4906db98b04e3290b6
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-09-04 22:48:39 +0200

    2 com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ export class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Bettie Karson', number: '237-41-56' },
     ],
     filter: '',
   };
